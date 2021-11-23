06c2336c7b9e016f2eb587c87f4cabbcae38ed6c
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-11-23 12:57:42 -0600

    mutiations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+    }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}
+`;
