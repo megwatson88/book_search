dac1cc892984515b212a3e25ea2a4c9d48217813
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-11-23 13:34:17 -0600

    readme

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,7 +6,7 @@ import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 import { useMutation} from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutation'
+import { LOGIN_USER } from '../utils/mutations'
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
